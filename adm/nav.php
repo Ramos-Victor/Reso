3f0919aafb37b273c6da1a6b6cf0dd2f3205f6a7
Ramos-Victor <vm02oliveiraramos@gmail.com>
2024-09-17 01:37:28 -0300

    Inicio front-end Sidebar

---
+++ adm/nav.php
@@ -0,0 +1,34 @@
+
+ <div id="sidebar">
+    <div>
+      <h4 class="text-white">Resolut.on</h4>
+      <ul class="nav flex-column">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Dashboard</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Chamados</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Cadastrar Sala</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Cadastrar Equipamento</a>
+        </li>
+      </ul>
+    </div>
+   
+    <ul class="nav flex-column">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Configurações</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Perfil</a>
+      </li>
+    </ul>
+  </div>
+
+  <div id="main-content">
+    <h1>Conteúdo principal</h1>
+    <p>Aqui vai o conteúdo da página. A sidebar fica fixa à esquerda.</p>
+  </div>
