be7fb313af036c224cd99626f22cd9b387509cf5
Ramos-Victor <vm02oliveiraramos@gmail.com>
2024-11-19 00:08:53 -0300

    fonte card

--- Views/models/painel/listar_ajax.php
+++ Views/models/painel/listar_ajax.php
@@ -20,7 +20,7 @@ require_once 'function.php';
     <div class="card mb-3"
         style="width: 400px; height: 400px; border: 5px solid <?= $cor ?>; background-color: transparent; border-radius: 15px;">
         <div class="card-header text-center"
-            style="font-size: 1.2rem; color: <?= $cor ?>; background-color: transparent; border-bottom: none;">
+            style="font-size: 2rem; color: <?= $cor ?>; background-color: transparent; border-bottom: none;">
             <?= $status ?>
         </div>
         <div class="card-body d-flex flex-column justify-content-center align-items-center">
