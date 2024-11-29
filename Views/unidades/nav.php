0dbd13b1a24c74c330dbbe0b49c3134b58ff5384
Ramos-Victor <vm02oliveiraramos@gmail.com>
2024-11-29 03:38:34 -0300

    justify-content-end

--- Views/unidades/nav.php
+++ Views/unidades/nav.php
@@ -43,7 +43,7 @@ nav {
         aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse justify-content-between" id="navbarNavDropdown">
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
         <ul class="navbar-nav">
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false"
